408859c07833fe2524b20f8353015ccea3de06f9
Dominic Brady <dombrady1993@gmail.com>
2017-11-13 22:39:11 +0000

    Log Timestamps

--- logger.js
+++ logger.js
@@ -3,8 +3,14 @@ const log4js = require("log4js");
 //Set up logging
 log4js.configure({
   appenders: {
-    out: { type: "console" },
-    app: { type: "file", filename: "logs/info.log"},
+    out: { type: "console", layout: {
+      type: "pattern",
+      pattern: "%[%d{yyyy/MM/dd-hh:mm:ss} [%p]%] %m"
+    } },
+    app: { type: "file", layout: {
+      type: "pattern",
+      pattern: "%[%d{yyyy/MM/dd-hh:mm:ss} [%p]%] %m"
+    }, filename: "logs/info.log"},
   },
   categories: {
     default: { appenders: [ "out", "app" ], level: "trace" }
