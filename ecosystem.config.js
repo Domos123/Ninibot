0ec69d4b666b4533a809682aec54c83220b09c7d
Dominic Brady <dombrady1993@gmail.com>
2017-11-19 23:13:02 +0000

    Cluster mode disabled

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     {
       name            : "Agender Bot",
       script          : "./bot.js",
-      instances       : "max",
-      exec_mode       : "cluster",
       watch           : false,
       node_args       : "--harmony",
       log_date_format : "YYYY-MM-DD HH:mm",
